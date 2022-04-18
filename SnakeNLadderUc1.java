e4d3d1dace8fbc3878baaa7fe0d06a687dd54cc9
Rajudv19 <rajudv1997@gmail.com>
2022-04-18 21:42:56 +0530

    Welcome Snack & Ladded code

---
+++ SnakeNLadderUc1.java
@@ -0,0 +1,12 @@
+package com.BridgeLabs.day4;
+
+public class SnakeNLadderUc1
+{
+	static final int START_POSITION = 0;
+
+    public static void main(String[] args) {
+        // welcome to snake ladder program.
+        System.out.println("Welcome to snake & ladder game");
+        System.out.println("Player will start from zero position.");
+    }
+}
